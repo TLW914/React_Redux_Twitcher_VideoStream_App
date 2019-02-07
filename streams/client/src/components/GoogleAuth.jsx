6ce436523256a365c8f580c0bf423a67a658a785
Tyler Watson <leggettwatson.tyler@gmail.com>
2019-02-07 15:01:53 -0800

    wired up buttons for Google oAuth sign in and sign out in react prior to refactor with redux

--- streams/client/src/components/GoogleAuth.jsx
+++ streams/client/src/components/GoogleAuth.jsx
@@ -28,13 +28,31 @@ class GoogleAuth extends React.Component {
         this.setState( {isSignedIn: this.auth.isSignedIn.get() });
     };
 
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+
     renderAuthButton() {
         if (this.state.isSignedIn === null){
-            return <div>dunno if signed in</div>
+            return null;
         } else if (this.state.isSignedIn){
-            return <div>signed in</div>
+            return (
+                <button onClick={this.onSignOut} className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
         } else {
-            return <div>i am not signed in</div>
+            return (
+            <button onClick={this.onSignIn} className="ui red google button">
+                <i className="google icon" />
+                Sign in with Google
+            </button>
+            );
         }
     }
 
