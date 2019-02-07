a6f34858176bc424a40cc900ca63330f1b3cb135
Tyler Watson <leggettwatson.tyler@gmail.com>
2019-02-07 14:49:55 -0800

    google oauth component working

--- streams/client/src/components/GoogleAuth.jsx
+++ streams/client/src/components/GoogleAuth.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import CLIENT_ID from './GoogleOAuthClientId.jsx';
+import CLIENT_ID from '../GoogleClientIDs/GoogleOAuthClientId.jsx';
 
 class GoogleAuth extends React.Component { 
+
+    state = { 
+        isSignedIn: null
+    }
+
     componentDidMount() {
         window.gapi.load('client: auth2', () => {
             window.gapi.client.init(
@@ -9,14 +14,34 @@ class GoogleAuth extends React.Component {
                     clientId: CLIENT_ID,
                     scope: 'email'
                 }
-            )
+            ).then(() => {
+            // saves reference to auth object on component class
+            this.auth = window.gapi.auth2.getAuthInstance();
+            //update componenet level state
+            this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+            this.auth.isSignedIn.listen(this.onAuthChange);
+            });
         });
+    }
+
+    onAuthChange = () => {
+        this.setState( {isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null){
+            return <div>dunno if signed in</div>
+        } else if (this.state.isSignedIn){
+            return <div>signed in</div>
+        } else {
+            return <div>i am not signed in</div>
+        }
     }
 
     render() {
         return (
             <div>
-                Google Auth
+                {this.renderAuthButton()}
             </div>
         );
     }
--- streams/client/src/components/streams/StreamEdit.jsx
+++ streams/client/src/components/streams/StreamEdit.jsx
@@ -9,3 +9,4 @@ const StreamEdit = () => {
 };
 
 export default StreamEdit;
+
