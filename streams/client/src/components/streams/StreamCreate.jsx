486120d7f19f0447195ac845aa234b143f4aa81f
Tyler Watson <leggettwatson.tyler@gmail.com>
2019-02-08 15:38:14 -0800

    refactored error messages to display on touch

--- streams/client/src/components/streams/StreamCreate.jsx
+++ streams/client/src/components/streams/StreamCreate.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput(formProps){
+    renderError(meta){
+        if (meta.touched && meta.error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{meta.error}</div>
+                </div>
+            )
+        }
+    }
+
+    renderInput = (formProps) => {
         //console.log('props from renderInput from field component in render func', formProps)
         //console.log('meta property in renderInput', formProps.meta)
         return (
@@ -14,11 +24,12 @@ class StreamCreate extends React.Component {
                         //adds them as props to the input element
                         //or destructure {input} arg and use {...input}
                     {...formProps.input}
+                    autoComplete="off"
                     //longhand version
                         // onChange={formProps.input.onChange} 
                         // value={formProps.input.value}
                 />
-                <div>{formProps.meta.error}</div>
+                {this.renderError(formProps.meta)}
             </div>
         );
     }
@@ -33,7 +44,7 @@ class StreamCreate extends React.Component {
         <div>
             {/* handleSubmit is callback function that is a property on reduxForm
             pass in our own callback into handleSubmit */}
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)}className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 {/* name property is required, need text input to appear, 
                 Field does not know how to render anything in DOM unless 
                 asign a prop to it as component or a function for the field to call 
