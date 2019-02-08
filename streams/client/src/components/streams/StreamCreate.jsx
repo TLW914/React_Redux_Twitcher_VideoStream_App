7e31a05c7ef3749f20aded1776b3cf3ce12e3440
Tyler Watson <leggettwatson.tyler@gmail.com>
2019-02-08 14:40:27 -0800

    implemented redux-form in StreamCreate component

--- streams/client/src/components/streams/StreamCreate.jsx
+++ streams/client/src/components/streams/StreamCreate.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return (
+class StreamCreate extends React.Component {
+    renderInput(formProps){
+        console.log('props from field component in render func', formProps)
+        return (
+            <div className="field">
+            <label>{formProps.label}</label>
+                <input 
+                    //shortcut version 
+                        //takes formProps input property and ALL values
+                        //adds them as props to the input element
+                        //or destructure {input} arg and use {...input}
+                    {...formProps.input}
+                    //longhand version
+                        // onChange={formProps.input.onChange} 
+                        // value={formProps.input.value}
+                />
+            </div>
+        );
+    }
+
+    render() {
+        console.log('props on reduxForm reducer', this.props)
+        return (
         <div>
-            StreamCreate
+            <form className="ui form">
+                {/* name property is required, need text input to appear, 
+                Field does not know how to render anything in DOM unless 
+                asign a prop to it as component or a function for the field to call 
+                that returns an element for the field to show
+                Field element also doesn't know what to do with props label so passes
+                it into renderInput and can receive it as a property
+                 */}
+                <Field name="title" component={this.renderInput} label="Enter Title"/>
+                <Field name="description" component={this.renderInput} label="Enter Description"/>
+            </form>
         </div>
-    );
-};
+        );
+    }
+}
 
-export default StreamCreate;
+// reduxForm works same way connect works--do not need reducers or map state to props - handles that
+// takes in configuration object
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+//imported named reducer from redux-form and renamed it
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
