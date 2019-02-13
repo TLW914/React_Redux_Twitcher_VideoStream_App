4493c6a97ac476828d49547ebf2468398ef40c70
Tyler Watson <leggettwatson.tyler@gmail.com>
2019-02-12 17:41:30 -0800

    implemented reducers for FETCH_STREAM, FETCH_STREAMS, CREATE_STREAM, EDIT_STREAM, DELETE_STREAM action creators

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 //imported named reducer from redux-form and renamed it
 import { reducer as formReducer } from 'redux-form';
-import authReducer from './authReducer';
+import authReducer from './authReducer.js';
+import streamReducer from './streamReducer.js';
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
